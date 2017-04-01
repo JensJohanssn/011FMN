8b7bc9d1039627ebea24b99bd0509531a4821755
Jens Johansson <jens.johanssn@gmail.com>
2017-04-01 16:43:51 +0200

    Task 2

--- ComputationalPart1.m
+++ ComputationalPart1.m
@@ -2,9 +2,9 @@
 clc
 clear all
 
-L = [3 3 3 3 3 3];
 b = 15;
 d = 1;
+L = [3 3 3 3 3 3];
 
 geval(L, b, d)
 
@@ -26,3 +26,31 @@ L = [7.5 7.5 7.5 7.5 7.5 7.5];
 geval(L, b, d)
 
 %% Task 2
+clc
+clear all
+
+a = 10;
+b = 15;
+d = 1;
+L = [11.5 11.5 11.5 11.5 11.5 11.5];
+
+G = geval(L, b ,d)
+
+h = G(1,:);
+X_P = G(2,:);
+Y_P = G(3,:);
+
+X_T = [2.5 3.2 -1.9] %Hittar inte bra värden
+stf(a, h , X_P, Y_P, X_T)
+
+
+
+
+
+
+
+
+
+
+
+
